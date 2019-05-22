ee23ae2489fa0b0953137d179859546091b98d85
Savannah C <sgcorrell@gmail.com>
2019-05-22 19:42:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,17 +43,6 @@ class Student
     new_student
   end
 
-  def self.all
-    sql = <<-SQL
-      SELECT *
-      FROM students
-    SQL
-    
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
-  end
-
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
