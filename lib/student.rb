61d2c4f97baadb8849de10b032dcc1f30c5de097
Savannah C <sgcorrell@gmail.com>
2019-05-22 19:46:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,8 +40,10 @@ class Student
     end
   end
   
-  def self.create
-    
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
   end
   
   def self.new_from_db(row)
