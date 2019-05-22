5b84771640358624be0c85c4b877971f6b60a0ec
Savannah C <sgcorrell@gmail.com>
2019-05-22 19:43:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,6 +33,7 @@ class Student
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
   
   def self.new_from_db(row)
