7a89b56ad58ae77e67a97db190a3c473c0bc31e7
Savannah C <sgcorrell@gmail.com>
2019-05-22 19:45:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,6 +27,9 @@ class Student
   end
   
   def save
+    if self.id
+      self.update
+    else
     sql = <<-SQL
       INSERT INTO students (name, grade) 
       VALUES (?, ?)
@@ -34,6 +37,7 @@ class Student
 
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    end
   end
   
   def self.create
