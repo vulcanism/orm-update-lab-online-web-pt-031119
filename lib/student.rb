395baa8b46ffb34be5cca6c1bf6bcc4060bf0037
Savannah C <sgcorrell@gmail.com>
2019-05-22 19:52:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,9 +4,9 @@ class Student
   attr_accessor :id, :name, :grade
   
   def initialize(name, grade, id=nil)
-    @id = id
     @name = name
     @grade = grade
+    @id = id
   end
   
   def self.create_table
